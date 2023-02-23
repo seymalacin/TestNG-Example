c3343f1d090879acf57a1e4e312bd3688c47a026
seymalacin <seymallacinn@gmail.com>
2023-02-23 21:03:52 +0300

    23.02.23

---
+++ src/test/java/tests/YeniDriver.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class YeniDriver {
+    @Test
+    public void testName() {
+        Driver.getDriver().get(ConfigReader.getProperty("amazon_url"));
+        Driver.closeDriver();
+        //Driver.getDriver().get("https://techproeducation.com");
+        //Driver.getDriver().get("https://facebook.com");
+
+    }
+}
